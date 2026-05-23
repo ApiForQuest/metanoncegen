930809265d779da5d538e77c2f69833fd8604381
ScriptSkiddie <freshburgers12@gmail.com>
2026-05-23 13:08:53 +0100

    Update register.js

--- register.js
+++ register.js
@@ -18,8 +18,18 @@ const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
     try {
-        console.log("Deploying commands:", commands);
+        console.log("Deploying commands...");
 
+        // OPTIONAL: clear old commands
+        await rest.put(
+            Routes.applicationGuildCommands(
+                process.env.CLIENT_ID,
+                process.env.GUILD_ID
+            ),
+            { body: [] }
+        );
+
+        // IMPORTANT: register new commands
         await rest.put(
             Routes.applicationGuildCommands(
                 process.env.CLIENT_ID,
