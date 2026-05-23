00103b1c23c2b0c08195cce60ab606d666ac75c1
ScriptSkiddie <freshburgers12@gmail.com>
2026-05-23 12:37:18 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -18,9 +18,12 @@ const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
     await rest.put(
-        Routes.applicationCommands(process.env.CLIENT_ID),
+        Routes.applicationGuildCommands(
+            process.env.CLIENT_ID,
+            process.env.GUILD_ID
+        ),
         { body: commands }
     );
 
-    console.log("Commands registered");
+    console.log("✅ Guild commands registered");
 })();
